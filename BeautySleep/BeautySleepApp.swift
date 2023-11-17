9f7a489b0b227c59e55c2be62f495134880c1aca
CheahWH <78494351+CheahWH@users.noreply.github.com>
2023-11-17 02:42:06 -0800

    Initial Commit

---
+++ BeautySleep/BeautySleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautySleepApp.swift
+//  BeautySleep
+//
+//  Created by Cheah Wen Hui on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeautySleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
