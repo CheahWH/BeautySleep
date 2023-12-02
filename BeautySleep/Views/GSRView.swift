ae36c11eac063922a08e90e548a8e82a03599a71
row3boat <67637128+row3boat@users.noreply.github.com>
2023-12-01 21:07:10 -0800

    GSRView
    GSRView now takes dummy data

--- BeautySleep/Views/GSRView.swift
+++ BeautySleep/Views/GSRView.swift
@@ -5,11 +5,36 @@
 //  Created by Cheah Wen Hui on 29/11/2023.
 //
 
+
+//TODO:
+//READ IN SENSOR DATA
+//PLOT DATA IN CHART
 import SwiftUI
+import Charts
+
+struct SleepData{
+    let id : Int
+    let time: Int
+    let er: Int
+}
+
+var data : [SleepData] = [
+    SleepData(id: 1, time: 5, er: 492),
+    SleepData(id: 2, time: 4, er: 648),
+    SleepData(id: 3, time: 3, er: 582),
+    SleepData(id:4, time: 2, er: 436),
+]
 
 struct GSRView: View {
     var body: some View {
-        Text("Login")
+        Chart(data, id: \.id) {
+                PointMark(
+                    x: .value("Wing Length", $0.time),
+                    y: .value("Wing Width", $0.er)
+                )
+            }
+        .chartXAxisLabel("Time")
+        .chartYAxisLabel("Resistence")
     }
 }
 
--- BeautySleep/Views/HomeView.swift
+++ BeautySleep/Views/HomeView.swift
@@ -67,6 +67,9 @@ struct HomeView: View {
             if let sleepSample = manager.selectedSleepSample {
                 SleepDetailsView(sleepSample: sleepSample)
             }
+            if let sleepSample = manager.selectedSleepSample {
+                SleepDetailsView(sleepSample: sleepSample)
+            }
         }
     }
 }
