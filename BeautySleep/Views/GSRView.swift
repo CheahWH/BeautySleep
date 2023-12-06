937d294e7635a0766386a4a37abefe601ff62371
row3boat <67637128+row3boat@users.noreply.github.com>
2023-12-06 15:54:46 -0800

    BadVersion
    1) Fetch from DB implemented
    2) Broke some logic oops.
    
    will try to merge to keep 1) and remove 2)

--- BeautySleep/Views/GSRView.swift
+++ BeautySleep/Views/GSRView.swift
@@ -10,128 +10,224 @@
 //READ IN SENSOR DATA
 //PLOT DATA IN CHART
 import SwiftUI
+import Firebase
+
+let database = Database.database()
 
 struct SleepData {
-    let id: Int
+    let id: String
     let time: CGFloat
     let er: CGFloat
 }
 
-var data: [SleepData] = [
-    SleepData(id: 1, time: 1, er: 492),
-    SleepData(id: 2, time: 2, er: 648),
-    SleepData(id: 3, time: 3, er: 582),
-    SleepData(id: 4, time: 4, er: 436),
-    SleepData(id: 5, time: 5, er: 436),
-    SleepData(id: 6, time: 6, er: 500),
-    SleepData(id: 7, time: 7, er: 300),
-    SleepData(id: 8, time: 8, er: 212),
-    SleepData(id: 9, time: 9, er: 250),
-    SleepData(id: 10, time: 10, er: 190),
-]
+func readDataForPastDay(completion: @escaping ([SleepData]?) -> Void) {
+    let currentDate = Date()
+    let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: currentDate)!
+
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy:MMdd:HHmm:ss"
+
+    let ref = Database.database().reference(withPath: "LJ/data")
+
+    // Start and end timestamps for the day
+    let startTimestamp = dateFormatter.string(from: yesterday)
+    let endTimestamp = dateFormatter.string(from: currentDate)
+
+    // Adjust these values based on the desired chunk size
+    let pageSize = 50
+    var currentPage = 0
+
+    // Function to fetch data for the current page
+    func fetchDataForPage(page: Int, completion: @escaping ([SleepData]?) -> Void) {
+        let query = ref.queryOrderedByKey()
+            .queryStarting(atValue: startTimestamp)
+            .queryEnding(atValue: endTimestamp)
+            .queryLimited(toFirst: UInt(pageSize * (page + 1)))  // Adjust limit based on page size
+
+        query.observeSingleEvent(of: .value, with: { snapshot in
+            guard let allData = snapshot.value as? [String: [String: Any]] else {
+                print("No data found for the past day")
+                completion(nil)
+                return
+            }
+
+            var sleepDataArray: [SleepData] = []
+
+            for (timestamp, entry) in allData {
+                print("Entry Timestamp: \(timestamp), Entry: \(entry)")
+
+                if let er = entry["gsrAverage"] as? CGFloat,
+                   let time = entry["timestamp"] as? CGFloat {
+
+                    let sleepData = SleepData(id: timestamp, time: Double(time), er: Double(er))
+                    sleepDataArray.append(sleepData)
+                    print(sleepData)
+                }
+            }
+
+            completion(sleepDataArray)
+            print("data displayed")
+        }) { error in
+            print("Error reading data:", error.localizedDescription)
+            completion(nil)
+        }
+    }
+
+    // Fetch the first page
+    fetchDataForPage(page: currentPage) { sleepDataArray in
+        if let sleepDataArray = sleepDataArray {
+            completion(sleepDataArray)
+        } else {
+            completion(nil)
+        }
+    }
+}
+
+
+
 
 struct GSRView: View {
+    @State private var loaded = false
+    @State private var data: [SleepData] = []
+    
     var body: some View {
         VStack {
-            Spacer()
-            HStack{
-                Spacer()
+            if !loaded {
+                Text("Loading data...")
+                    .onAppear {
+                        readDataForPastDay { sleepDataArray in
+                            if let sleepDataArray = sleepDataArray {
+                                self.data = Array(sleepDataArray.prefix(20))
+                            }
+                            loaded = true
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){
+                            print("loading")
+                        }
+                    }
+            }else {
                 LineChart(data: data)
-                    .padding()
-                    .frame(width: 350, height: 700)
-                Spacer()
             }
-            Spacer()
         }
     }
 }
 
-struct GSRView_Previews: PreviewProvider {
-    static var previews: some View {
-        GSRView()
+    
+    struct GSRView_Previews: PreviewProvider {
+        static var previews: some View {
+            GSRView()
+        }
     }
-}
+    
+//struct LineChart: View {
+//    let data: [SleepData]
+//    
+//    var body: some View {
+//        GeometryReader { geometry in
+//            let maxX = data.map(\.time).max() ?? 0
+//            let maxY = data.map(\.er).max() ?? 0
+//            
+//            let xScale = maxX > 0 ? geometry.size.width / maxX : 0
+//            let yScale = maxY > 0 ? geometry.size.height / maxY : 0
+//            
+//            // Y-axis
+//            Path { path in
+//                path.move(to: CGPoint(x: 30, y: 0))
+//                path.addLine(to: CGPoint(x: 30, y: geometry.size.height - 20))
+//            }
+//            .stroke(lineWidth: 1)
+//            .foregroundColor(.black)
+//            
+//            // X-axis
+//            Path { path in
+//                path.move(to: CGPoint(x: 30, y: geometry.size.height - 20))
+//                path.addLine(to: CGPoint(x: geometry.size.width - 30, y: geometry.size.height - 20))
+//            }
+//            .stroke(lineWidth: 1)
+//            .foregroundColor(.black)
+//            
+//            // Data points and labels
+//            ForEach(data, id: \.id) { point in
+//                let x = xScale * (point.time - data[0].time) + 30 // Adjust x position based on the first timestamp
+//                let y = geometry.size.height - yScale * point.er - 20
+//                
+//                Path { path in
+//                    path.addEllipse(in: CGRect(x: x - 5, y: y - 5, width: 10, height: 10))
+//                }
+//                .foregroundColor(.red)
+//                
+//                Text("\(Int(point.er))")
+//                    .position(x: x, y: y - 20)
+//            }
+//            
+//            // Y-axis labels
+//            ForEach(1..<6, id: \.self) { i in
+//                let y = yScale * CGFloat(i) * maxY / 5 - 20
+//                Text("\(Double(y))")
+//                    .position(x: 15, y: geometry.size.height - y)
+//            }
+//            
+//            // X-axis labels (timestamps)
+//            ForEach(data, id: \.id) { point in
+//                let x = xScale * (point.time - data[0].time) + 30
+//                Text("\(Int(point.time))")
+//                    .position(x: x, y: geometry.size.height - 10)
+//            }
+//            
+//            // Y-axis label
+//            Text("Resistance (Units)")
+//                .rotationEffect(.degrees(-90))
+//                .position(x: -20, y: geometry.size.height / 2)
+//            
+//            // X-axis label
+//            Text("Time (Seconds)")
+//                .position(x: geometry.size.width / 2, y: geometry.size.height + 20)
+//            
+//            // Connecting lines
+//            Path { path in
+//                for (index, point) in data.enumerated() {
+//                    let x = xScale * (point.time - data[0].time) + 30
+//                    let y = geometry.size.height - yScale * point.er - 20
+//                    
+//                    if index == 0 {
+//                        path.move(to: CGPoint(x: x, y: y))
+//                    } else {
+//                        path.addLine(to: CGPoint(x: x, y: y))
+//                    }
+//                }
+//            }
+//            .stroke(lineWidth: 2)
+//            .foregroundColor(.blue)
+//        }
+//    }
+//}
 
 struct LineChart: View {
-    let data: [SleepData]
+    var data: [SleepData]
 
     var body: some View {
-        GeometryReader { geometry in
-            let maxX = data.map(\.time).max() ?? 0
-            let maxY = data.map(\.er).max() ?? 0
-
-            let xScale = geometry.size.width / maxX
-            let yScale = geometry.size.height / maxY
-
-            // Y-axis
-            Path { path in
-                path.move(to: CGPoint(x: 30, y: 0))
-                path.addLine(to: CGPoint(x: 30, y: geometry.size.height - 20))
-            }
-            .stroke(lineWidth: 1)
-            .foregroundColor(.black)
-
-            // X-axis
-            Path { path in
-                path.move(to: CGPoint(x: 30, y: geometry.size.height - 20))
-                path.addLine(to: CGPoint(x: geometry.size.width - 30, y: geometry.size.height - 20))
-            }
-            .stroke(lineWidth: 1)
-            .foregroundColor(.black)
-
-            // Data points and labels
-            ForEach(data, id: \.id) { point in
-                let x = xScale * point.time + 30
-                let y = geometry.size.height - yScale * point.er - 20
-
+        VStack {
+            GeometryReader { geometry in
                 Path { path in
-                    path.addEllipse(in: CGRect(x: x - 5, y: y - 5, width: 10, height: 10))
-                }
-                .foregroundColor(.red)
-
-                Text("\(Int(point.er))")
-                    .position(x: x, y: y - 20)
-            }
-
-            // Y-axis labels
-            ForEach(1..<6, id: \.self) { i in
-                let y = yScale * CGFloat(i) * maxY / 5 - 20
-                Text("\(Int(y))")
-                    .position(x: 15, y: geometry.size.height - y)
-            }
-
-            // X-axis labels
-            ForEach(1..<Int(maxX) + 1, id: \.self) { i in
-                let x = xScale * CGFloat(i) + 30
-                Text("\(i)")
-                    .position(x: x, y: geometry.size.height - 10)
-            }
-
-            // Y-axis label
-            Text("Resistance (Units)")
-                .rotationEffect(.degrees(-90))
-                .position(x: -20, y: geometry.size.height / 2)
-
-            // X-axis label
-            Text("Time (Seconds)")
-                .position(x: geometry.size.width / 2, y: geometry.size.height + 20)
-
-            // Connecting lines
-            Path { path in
-                for (index, point) in data.enumerated() {
-                    let x = xScale * point.time + 30
-                    let y = geometry.size.height - yScale * point.er - 20
-
-                    if index == 0 {
-                        path.move(to: CGPoint(x: x, y: y))
-                    } else {
-                        path.addLine(to: CGPoint(x: x, y: y))
+                    for i in 0..<data.count {
+                        let x = CGFloat(i) * (geometry.size.width / CGFloat(data.count - 1))
+                        let y = CGFloat(data[i].er) * geometry.size.height
+                        let point = CGPoint(x: x, y: y)
+                        
+                        if i == 0 {
+                            path.move(to: point)
+                        } else {
+                            path.addLine(to: point)
+                        }
                     }
                 }
+                .stroke(Color.blue, lineWidth: 2)
             }
-            .stroke(lineWidth: 2)
-            .foregroundColor(.blue)
+            .frame(height: 200)
         }
     }
 }
+
+
+    
 
