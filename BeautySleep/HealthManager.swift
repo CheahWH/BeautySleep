e2c8be29a84464948cbb7d557705b57fd17a96a7
CheahWH <78494351+CheahWH@users.noreply.github.com>
2023-12-07 00:44:44 -0800

    fixed healthkit sleep data
    got rid of time in bed, sleep details is 24hrs (1 whole day), last nights sleep is the last 36hours

--- BeautySleep/HealthManager.swift
+++ BeautySleep/HealthManager.swift
@@ -39,11 +39,12 @@ class HealthManager: ObservableObject {
         }
     }
 
+    //track the last 36hours from NOW
     func fetchLastNightSleep() {
         let sleepType = HKCategoryType.categoryType(forIdentifier: .sleepAnalysis)!
-        let yesterdayStart = Calendar.current.date(byAdding: .day, value: -1, to: Date.startOfDay)!
-        let yesterdayEnd = Date.startOfDay
-        let predicate = HKQuery.predicateForSamples(withStart: yesterdayStart, end: yesterdayEnd)
+        let startDate = Calendar.current.date(byAdding: .hour, value: -36, to: Date())!
+        let endDate = Date()
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate)
         let query = HKSampleQuery(sampleType: sleepType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { _, samples, error in
             guard let samples = samples as? [HKCategorySample], error == nil else {
                 print("Error fetching last night's sleep data")
@@ -56,8 +57,8 @@ class HealthManager: ObservableObject {
                 let hours = sample.endDate.timeIntervalSince(sample.startDate) / 3600
                 totalSleepHours += hours
             }
-
-            let activity = Activity(id: 0, title: "Last Night's Sleep", subtitle: "Hours", image: "moon.stars", amount: totalSleepHours.formattedString(), view: AnyView(GSRView()))
+            
+            let activity = Activity(id: 0, title: "Last Night's Sleep", subtitle: "Total:", image: "moon.stars", amount: totalSleepHours.formattedString(), view: AnyView(GSRView()))
             DispatchQueue.main.async {
                 self.activities["lastNightSleep"] = activity
             }
@@ -65,6 +66,7 @@ class HealthManager: ObservableObject {
 
         healthStore.execute(query)
     }
+
     
     func fetchLastNightSleepDetails() {
         let sleepType = HKCategoryType.categoryType(forIdentifier: .sleepAnalysis)!
@@ -77,7 +79,7 @@ class HealthManager: ObservableObject {
                 return
             }
 
-            // Assuming you want to display details for the first sample
+            //display details for the first sample
             if let firstSample = samples.first {
                 DispatchQueue.main.async {
                     self.selectedSleepSample = firstSample
@@ -87,82 +89,24 @@ class HealthManager: ObservableObject {
 
         healthStore.execute(query)
     }
-
-    func displayGSRData() {
-        let activity = Activity(id: 1, title: "GSR Data", subtitle: "Disturbances", image: "chart.xyaxis.line", amount: nil, view: AnyView(GSRView()))
-        DispatchQueue.main.async {
-            self.activities["GSRData"] = activity
-        }
-    }
 }
 
 extension Double {
     func formattedString() -> String {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.maximumFractionDigits = 2
+        let hours = Int(self)
+        let minutes = Int((self - Double(hours)) * 60)
 
-        if let formattedValue = numberFormatter.string(from: NSNumber(value: self)) {
-            return formattedValue
+        if hours > 0 {
+            if minutes > 0 {
+                return "\(hours)h \(minutes)m"
+            } else {
+                return "\(hours)h"
+            }
         } else {
-            return "N/A"
+            return "\(minutes)m"
         }
     }
 }
 
 
-
-
-
-/* //source code
-import Foundation
-import HealthKit
-
-extension Date{
-    static var startOfDay: Date{
-        Calendar.current.startOfDay(for: Date())
-    }
-}
-class HealthManager: ObservableObject{
- @Published var activities: [String: Activity] = [:]
-    let healthStore = HKHealthStore()
-    init(){
-        let steps = HKQuantity(.stepCount)
-        let healthTypes = Set = [steps]
-        Task{
-            do{
-                try await healthStore.requestAuthorization(toShare: [], read: healthTypes)
-            }catch{
-                print("error fetching health data! :(")
-            }
-        }
-    }
-    func fetchTodaySteps(){
-        let steps = HKQuantityType(.stepCount)
-        //let health kit know what day we seaching for
-        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
-        let query = HKStatisticsQuery(quantityType: steps, quantitySamplePredicate: predicate){_, result, error in guard let quantity = result?.sumQuantity(), error == nil else{
-                print("error fetching todays sleep data")
-                return
-            }
-            let stepCount = quantity.doubleValue(for: .count())
-            //step to be first index 0, calorie to be index 1
-            let activity = Activity(id: 0, title: "Today steps", subtitle: "Goal 10,000 ", image: "figure.walk", amount: stepCount.formattedString())
-            DispatchQueue.main.async{
-                self.activities["todaySteps"] = activity
-            }
-            print(stepCount.formattedString())
-        }
-        healthStore.execute(query)
-    }
-}
-extension Double{
-    func formattedString() -> String? {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.maximumFractionDigits = 0
-        return numberFormatter.string(from: NSNumber(value: self))
-    }
-}
-*/
 
